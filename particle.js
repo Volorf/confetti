e12179efe94da232758df135d89c823de3ff6625
Oleg Frolov <frolololeg@gmail.com>
2021-07-14 14:08:29 +0300

    cleaned up a bit

--- particle.js
+++ particle.js
@@ -6,71 +6,65 @@ class Particle
         this.y = loc.y
         this.z = loc.z
       
-      this.rotStep = 0.04
-      this.rotAcc = 0.0003
-      this.rotDirX = random([-1, 1])
-      this.rotDirY = random([-1, 1])
-      this.rotDirZ = random([-1, 1])
-      this.randRotX = random(TWO_PI)
-      this.randRotY = random(TWO_PI)
-      this.randRotZ = random(TWO_PI)
+        this.rotStep = 0.04
+        this.rotAcc = 0.0003
+        this.rotDirX = random([-1, 1])
+        this.rotDirY = random([-1, 1])
+        this.rotDirZ = random([-1, 1])
+        this.randRotX = random(TWO_PI)
+        this.randRotY = random(TWO_PI)
+        this.randRotZ = random(TWO_PI)
     
         this.scale = 0
         this.scaleStep = 0.05
 
+        this.locStep = 1
+        this.locAcc = 0.01
+        this.locDirX = random([-1, 1])
+        this.locDirY = random([-1, 1])
 
-      this.locStep = 1
-      this.locAcc = 0.01
-      this.locDirX = random([-1, 1])
-      this.locDirY = random([-1, 1])
-
-        this.locZMultiplier = random(3, 5)
+        this.locZMultiplier = random(3, 6)
 
         this.model = model
-      
-    //   print(this.randRotX) 
     }
 
     draw() 
     {
-      // Rot easings
-      if (this.rotStep >=0) 
-      {
-          this.randRotX += this.rotStep * this.rotDirX
-          this.randRotY += this.rotStep * this.rotDirY
-        //   this.randRotZ += this.rotStep * this.rotDirZ
-          
-          this.rotStep -= this.rotAcc
-      }
+        // Rot easings
+        if (this.rotStep >=0) 
+        {
+            this.randRotX += this.rotStep * this.rotDirX
+            this.randRotY += this.rotStep * this.rotDirY
+            this.rotStep -= this.rotAcc
+        }
 
-      // Loc easings
-      if (this.locStep >= 0)
-      {
-        this.locStep -= this.locAcc
+        // Loc easings
+        if (this.locStep >= 0)
+        {
+            this.locStep -= this.locAcc
 
-        this.x += this.locStep * this.locDirX
-        this.y += this.locStep * this.locDirY
-        this.z += this.locStep * this.locZMultiplier
-      }
+            this.x += this.locStep * this.locDirX
+            this.y += this.locStep * this.locDirY
+            this.z += this.locStep * this.locZMultiplier
+        }
 
-      if (this.scale <= 1)
-      {
-          this.scale += this.scaleStep
-      }
+        if (this.scale <= 1)
+        {
+            this.scale += this.scaleStep
+        }
 
-      push()
-      translate(this.x, this.y, this.z)
-      rotateX(this.randRotX)
-      rotateY(this.randRotY)
-    //   rotateZ(this.randRotZ)
-      fill(255)
-    //   plane(20,20,20)
-    // ambientLight(0);
+        push()
+        
         noStroke()
-        // emissiveMaterial(130, 230, 0);
-        ambientMaterial(10, 0, 10)
+        ambientMaterial(10, 10, 10)
+        
+        translate(this.x, this.y, this.z)
+        rotateX(this.randRotX)
+        rotateY(this.randRotY)
         scale(this.scale)
+
         model(this.model)
+        
         pop()
     }
   
--- sketch.js
+++ sketch.js
@@ -1,20 +1,47 @@
-let boxes = []
+
 const PARTICLE_AMOUNT = 50
 const LOC_MARGIN = 100
+const BG_COLOR = 255
 
 let cameraXOffset = 0
 let cameraYOffset = 0
 
 let models =[]
+let particles = []
 
-function preload() {
-  // let m1 = loadModel('models/m1.obj');
-  // models.push(m1)
-  // let m2 = loadModel('models/m2.obj');
-  // models.push(m2)
-  let m3 = loadModel('models/m3.obj');
+// Custom funcs
+function generateParticles(particleAmount, locMargin)
+{
+  let _particles = []
+  for (let i = 0; i < particleAmount; i++)
+  {
+    let randX = random(-width /2 + locMargin, width / 2 - locMargin)
+    let randY = random(-height /2 + locMargin, height / 2 - locMargin)
+    let randZ = random(locMargin)
+    let posVec = createVector(randX, randY, randZ)
+    let particle = new Particle(posVec, random(models))
+    _particles.push(particle)
+  }
+  return _particles
+}
+
+function setCameraMotion()
+{
+  cameraXOffset += (mouseX - pmouseX) * 0.1
+  cameraYOffset += (mouseY - pmouseY) * 0.1
+  camera(cameraXOffset, cameraYOffset, 800)
+}
+
+// Lib's funcs
+function preload() 
+{
+  let m1 = loadModel('models/m1.obj')
+  models.push(m1)
+  let m2 = loadModel('models/m2.obj')
+  models.push(m2)
+  let m3 = loadModel('models/m3.obj')
   models.push(m3)
-  let m4 = loadModel('models/m4.obj');
+  let m4 = loadModel('models/m4.obj')
   models.push(m4)
 }
 
@@ -22,34 +49,14 @@ function setup()
 {
   createCanvas(windowWidth, windowHeight, WEBGL)
   perspective(PI / 4.0, width / height, 0.1, 4000)
-  
-  
-  for (let i = 0; i < PARTICLE_AMOUNT; i++)
-  {
-    let randX = random(-width /2 + LOC_MARGIN, width / 2 - LOC_MARGIN)
-    let randY = random(-height /2 + LOC_MARGIN, height / 2 - LOC_MARGIN)
-    let randZ = random(LOC_MARGIN)
-    let posVec = createVector(randX, randY, randZ)
-    let box = new Particle(posVec, random(models))
-    boxes.push(box)
-  }
-
-  // box = new Particle(loc)
+  particles = generateParticles(PARTICLE_AMOUNT, LOC_MARGIN)
 }
 
 function draw() 
 {
-  background(255)
-  // ambientLight(255)
-  let dirX = (mouseX / width - 0.5) * 2;
-  let dirY = (mouseY / height - 0.5) * 2;
-  directionalLight(255, 255, 255, -dirX, -dirY, -1);
-  // let cameraXOffset = (map(mouseX, 0, width, -100, 100))
-  cameraXOffset += (mouseX - pmouseX) * 0.1
-  cameraYOffset += (mouseY - pmouseY) * 0.1
-  print(mouseX)
-  camera(cameraXOffset, cameraYOffset, 800)
-  boxes.forEach(box => box.draw())
+  background(BG_COLOR)
+  particles.forEach(box => box.draw())
+  setCameraMotion()
 }
 
 
