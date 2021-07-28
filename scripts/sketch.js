735ab8e9b557d6e0aba5118f96b99b424dff4f94
Oleg Frolov <frolololeg@gmail.com>
2021-07-28 10:27:18 +0300

    Added new models

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -15,11 +15,11 @@ function generateParticles(particleAmount, locMargin)
   let _particles = [];
   for (let i = 0; i < particleAmount; i++)
   {
-    let randX = random(-width /2 + locMargin, width / 2 - locMargin);
-    let randY = random(-height /2 + locMargin, height / 2 - locMargin);
-    let randZ = random(locMargin);
-    let posVec = createVector(randX, randY, randZ);
-    let particle = new Particle(posVec, random(models));
+    const RAND_X = random(-width /2 + locMargin, width / 2 - locMargin);
+    const RAND_Y = random(-height /2 + locMargin, height / 2 - locMargin);
+    const RAND_Z = random(locMargin);
+    const POS_VEC = createVector(RAND_X, RAND_Y, RAND_Z);
+    let particle = new Particle(POS_VEC, random(models));
     _particles.push(particle);
   }
   return _particles;
@@ -35,14 +35,22 @@ function setCameraMotion()
 // Lib's funcs
 function preload() 
 {
-  let m1 = loadModel('models/m1.obj');
-  models.push(m1);
-  let m2 = loadModel('models/m2.obj');
-  models.push(m2);
-  let m3 = loadModel('models/m3.obj');
-  models.push(m3);
-  let m4 = loadModel('models/m4.obj');
-  models.push(m4);
+  // const M_1 = loadModel('models/m1.obj');
+  // models.push(M_1);
+  // const M_2 = loadModel('models/m2.obj');
+  // models.push(M_2);
+  // const M_3 = loadModel('models/m3.obj');
+  // models.push(M_3);
+  // const M_4 = loadModel('models/m4.obj');
+  // models.push(M_4);
+  const M_5 = loadModel('models/m5.obj');
+  models.push(M_5);
+  const M_6 = loadModel('models/m6.obj');
+  models.push(M_6);
+  const M_7 = loadModel('models/m7.obj');
+  models.push(M_7);
+  const M_8 = loadModel('models/m8.obj');
+  models.push(M_8);
 }
 
 function setup() 
